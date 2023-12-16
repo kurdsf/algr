2b2a6b5ef4604656a30a017c923def4044ae8d94
Kurt Pieper <user.kurtpieper@protonmail.com>
2023-12-16 20:38:57 +0100

    Simplified repl for testing the parser

--- repl.cc
+++ repl.cc
@@ -1,37 +1,16 @@
 #include<iostream>
 #include<string>
-#include<cstring>
 
-#include"presedence.h"
+#include"lexer.h"
 #include"parser.h"
-#include"optimizer.h"
 
 
 int main(){
-    AST* ast;
-    while(1){
-        std::string input;
-        std::cout << "algr>";
-        std::getline(std::cin, input);
-
-        if(input == "q\n") {
-            break;
-        }
-
-        if(input[0] == '#'){
-            continue;
-        }
-        // TODO: Fix this workaround, make parse accept const char*.
-        char* tmp = new char[input.size() + 1];
-        strcpy(tmp, input.c_str());
-        tmp[input.size()] = '\0';
-        ast = parse(tmp);
-        delete[] tmp;
-        ast = optimize(ast);
-        putsAST(ast);
-        std::cout << '\n';
-        freeAST(ast);
-    }
-    return 0;
+    std::string line;
+    std::getline(std::cin, line);
+    std::queue<Token> tokens = tokenize(line);
+    Ast ast = parse(tokens);
+    std::cout << ast << '\n';
 }
+
  
