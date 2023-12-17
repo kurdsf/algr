8f3a23d09bacc3119299554f0d68560ad0bbbe70
Kurt Pieper <user.kurtpieper@protonmail.com>
2023-12-17 01:22:54 +0100

    Make parser report error location; check for error with end of input.

--- parser.cc
+++ parser.cc
@@ -7,21 +7,41 @@ static void expect(std::queue<Token>& tokens, Token front) {
         return;
     }
 
-    std::cerr << "algr: expected: " << front << " but got: " << tokens.front() << " instead\n";
-    exit(0);
+    if(tokens.size() > 0) {
+        for(size_t i = 0; i < tokens.front().pos; ++i) std::clog << ' ';
+        std::clog << "^\n";
+        std::clog << "algr: expected " << front << " but got " << tokens.front() << " instead\n";
+    } else {
+        std::clog << "algr: expected " << front << " but got end of input instead\n";
+    }
+    exit(1);
 }
 
 
+Ast parse_sum(std::queue<Token>& tokens);
 Ast parse_product(std::queue<Token>& tokens);
 Ast parse_primary(std::queue<Token>& tokens);
 
+
 Ast parse(std::queue<Token>& tokens) {
+    Ast res = parse_sum(tokens);
+    if(tokens.size() != 0) {
+        for(size_t i = 0; i < tokens.front().pos; ++i) std::clog << ' ';
+        std::clog << "^\n";
+        std::clog << "algr: expected end of input but got " << tokens.front() << " instead\n";
+        exit(1);
+    }
+    return res;
+
+}
+
+Ast parse_sum(std::queue<Token>& tokens) {
     Ast lhs = parse_product(tokens);
     if(tokens.size() > 0 
         && tokens.front().type == TokenType::Add || tokens.front().type == TokenType::Sub) {
         Token op = tokens.front();
         tokens.pop();
-        Ast rhs = parse(tokens);
+        Ast rhs = parse_sum(tokens);
         return {op, {lhs, rhs}};
 
     }
@@ -51,18 +71,24 @@ Ast parse_primary(std::queue<Token>& tokens) {
     }
 
     expect(tokens, {TokenType::Lparen});
-    Ast expr = parse(tokens);
+    Ast expr = parse_sum(tokens);
     expect(tokens, {TokenType::Rparen});
     return expr;
 }
 
 
+// Print the AST in lisp-style
 std::ostream& operator<<(std::ostream& os, const Ast& ast) {
-    os << ast.op << '{';
-    for(auto node: ast.nodes) {
-        os << node;
+    // Don't print brackeys when printing an atom (num or var).
+    if(ast.nodes.size() == 0) {
+        os << ast.op;
+        return os;
     }
-    os << '}';
+    os << '(' << ast.op << ' ';
+    for(const auto& node: ast.nodes) {
+        os << node << ' ';
+    }
+    os << ')';
     return os;
 }
 
